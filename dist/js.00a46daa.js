6dfe3717490790eb9b685bba1abe722ef8a7f6ef
HranW <57498766+HranW@users.noreply.github.com>
2020-05-28 21:17:37 -0400

    homepage attempt to make it look nice

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -288,7 +288,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59714" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60739" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
